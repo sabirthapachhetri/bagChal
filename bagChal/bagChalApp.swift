1f46ccd05a6dfb2500c9b091a47d2e62b1223cc7
Sabir Thapa Chhetri <sabir.thapa@esewa.com.np>
2023-09-30 07:38:54 +0545

    Initial Commit

---
+++ bagChal/bagChalApp.swift
@@ -0,0 +1,17 @@
+//
+//  bagChalApp.swift
+//  bagChal
+//
+//  Created by Sabir Thapa on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct bagChalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
