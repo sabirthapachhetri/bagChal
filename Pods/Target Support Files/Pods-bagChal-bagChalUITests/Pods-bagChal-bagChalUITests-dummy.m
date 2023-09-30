0d5cbcd0c404e69a3b3c0f479657f3a727f9714a
Sabir Thapa Chhetri <sabir.thapa@esewa.com.np>
2023-09-30 08:47:05 +0545

    Bagchal Project Setup

---
+++ Pods/Target Support Files/Pods-bagChal-bagChalUITests/Pods-bagChal-bagChalUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bagChal_bagChalUITests : NSObject
+@end
+@implementation PodsDummy_Pods_bagChal_bagChalUITests
+@end
--- bagChal/ContentView.swift
+++ bagChal/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var loginRegisterSelected = "Login"
+    var loginRegisterOptions = ["Login", "Register"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 48/255, green: 68/255, blue: 158/255)
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("Hello, There")
+                    .font(.system(size: 15, weight: .bold, design: .rounded))
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 1)
+                
+                Text("Welcome Back")
+                    .font(.system(size: 25, weight: .bold, design: .rounded))
+                    .foregroundColor(.white)
+
+                Picker("", selection: $loginRegisterSelected) {
+                    ForEach(loginRegisterOptions, id: \.self) {
+                        // do sth
+                        Text($0)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
         }
-        .padding()
     }
 }
 
